1ecd8e1a0eb8a9a4f9ffeb0254314b135d3071f8
Victor Andevert <victor.andevert@gmail.com>
2023-02-04 19:00:11 +0100

    wip

--- src/main/kotlin/com/ecommerce/payments/domain/PaymentService.kt
+++ src/main/kotlin/com/ecommerce/payments/domain/PaymentService.kt
@@ -3,20 +3,19 @@ package com.ecommerce.payments.domain
 class PaymentService(private val pspClient: PspClient, private val fraudClient: FraudClient) {
 
     fun makeA(payment: Payment): PaymentResult {
-        if (payment.isItNecessaryToEvaluateFraud()) {
-            return fraudClient.evaluate(payment).fold(
-                { PaymentResult.denied()},
-                {fraud ->
-                    if (fraud.score <= 5) {
-                        return pspClient.payWith(payment).fold(
-                            { PaymentResult.denied(fraud.score) },
-                            { PaymentResult.accepted(it, fraud.score)})
-                    } else PaymentResult.denied(fraud.score)
-                })
-        } else {
-            return pspClient.payWith(payment).fold(
-                {  PaymentResult.denied() },
-                {  PaymentResult.accepted(it)})
+        return when {
+            payment.isItNecessaryToEvaluateFraud() -> fraudClient.evaluate(payment).fold(
+                    {PaymentResult.denied()},
+                    {fraud ->
+                        if (fraud.score <= 5) {
+                            return pspClient.payWith(payment).fold(
+                                { PaymentResult.denied(fraud.score) },
+                                { PaymentResult.accepted(it, fraud.score)})
+                        } else return PaymentResult.denied(fraud.score)
+                    })
+            else -> pspClient.payWith(payment).fold(
+                    {  PaymentResult.denied() },
+                    {  PaymentResult.accepted(it)})
         }
     }
 }
