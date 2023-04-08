f76cb609fa0e805f4e61a9b421631d8ebe062df2
Victor Andevert <victor.andevert@gmail.com>
2023-04-08 21:03:53 +0200

    adapt tests for coroutines

--- src/test/kotlin/com/ecommerce/payments/feature/domain/PaymentServiceShould.kt
+++ src/test/kotlin/com/ecommerce/payments/feature/domain/PaymentServiceShould.kt
@@ -4,22 +4,23 @@ import arrow.core.Either.Right
 import arrow.core.Some
 import com.ecommerce.payments.domain.*
 import io.mockk.Called
-import io.mockk.every
+import io.mockk.coEvery
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import kotlin.test.Test
 
 class PaymentServiceShould {
 
     @Test
-    fun `call PSP client to make a payment when fraud score is below 6`() {
+    fun `call PSP client to make a payment when fraud score is below 6`() = runTest {
         val payment = Payment(SaleId("SALE123"), Amount.from("100000", "EUR"))
         val pspClient = mockk<PspClient> {
-            every { payWith(payment) } returns Right(PspResponse(Some(Reference("12222-2222-222")), "ACCEPTED"))
+            coEvery { payWith(payment) } returns Right(PspResponse(Some(Reference("12222-2222-222")), "ACCEPTED"))
         }
         val fraudClient = mockk<FraudClient> {
-            every { evaluate(payment) } returns Right(FraudResponse(5))
+            coEvery { evaluate(payment) } returns Right(FraudResponse(5))
         }
         val paymentService = PaymentService(pspClient, fraudClient)
 
@@ -32,11 +33,11 @@ class PaymentServiceShould {
     }
 
     @Test
-    fun `don't make a payment when fraud score is higher than 5`() {
+    fun `don't make a payment when fraud score is higher than 5`() = runTest {
         val payment = Payment(SaleId("SALE123"), Amount.from("100000", "EUR"))
         val pspClient = mockk<PspClient>(relaxed = true)
         val fraudClient = mockk<FraudClient> {
-            every { evaluate(payment) } returns Right(FraudResponse(8))
+            coEvery { evaluate(payment) } returns Right(FraudResponse(8))
         }
         val paymentService = PaymentService(pspClient, fraudClient)
 
@@ -47,10 +48,10 @@ class PaymentServiceShould {
     }
 
     @Test
-    fun `don't evaluate fraud when amount is below 100 EUR`() {
+    fun `don't evaluate fraud when amount is below 100 EUR`() = runTest {
         val payment = Payment(SaleId("SALE123"), Amount.from("20000", "EUR"))
         val pspClient = mockk<PspClient> {
-            every { payWith(payment) } returns Right(PspResponse(Some(Reference("12222-2222-222")), "ACCEPTED"))
+            coEvery { payWith(payment) } returns Right(PspResponse(Some(Reference("12222-2222-222")), "ACCEPTED"))
         }
         val fraudClient = mockk<FraudClient>(relaxed = true)
         val paymentService = PaymentService(pspClient, fraudClient)
@@ -62,10 +63,10 @@ class PaymentServiceShould {
     }
 
     @Test
-    fun `don't evaluate fraud when amount is below 50 USD`() {
+    fun `don't evaluate fraud when amount is below 50 USD`() = runTest {
         val payment = Payment(SaleId("SALE123"), Amount.from("25000", "USD"))
         val pspClient = mockk<PspClient> {
-            every { payWith(payment) } returns Right(PspResponse(Some(Reference("12222-2222-222")), "ACCEPTED"))
+            coEvery { payWith(payment) } returns Right(PspResponse(Some(Reference("12222-2222-222")), "ACCEPTED"))
         }
         val fraudClient = mockk<FraudClient>(relaxed = true)
         val paymentService = PaymentService(pspClient, fraudClient)
@@ -77,11 +78,11 @@ class PaymentServiceShould {
     }
 
     @Test
-    fun `deny payments due to fraud for non-accepted currencies`() {
+    fun `deny payments due to fraud for non-accepted currencies`() = runTest {
         val payment = Payment(SaleId("SALE123"), Amount.from("25000", "COP"))
         val pspClient = mockk<PspClient>(relaxed = true)
         val fraudClient = mockk<FraudClient> {
-            every { evaluate(payment) } returns Right(FraudResponse(10))
+            coEvery { evaluate(payment) } returns Right(FraudResponse(10))
         }
         val paymentService = PaymentService(pspClient, fraudClient)
 
