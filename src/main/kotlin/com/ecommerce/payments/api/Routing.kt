207f63c73bf564cdc5e27aed2202d5bc573f6889
Victor Andevert <victor.andevert@gmail.com>
2023-04-08 21:14:35 +0200

    run coroutines on different threads from the common thread pool

--- src/main/kotlin/com/ecommerce/payments/api/Routing.kt
+++ src/main/kotlin/com/ecommerce/payments/api/Routing.kt
@@ -1,26 +1,29 @@
 package com.ecommerce.payments.api
 
-import com.ecommerce.payments.domain.Amount
-import com.ecommerce.payments.domain.Payment
-import com.ecommerce.payments.domain.PaymentService
-import com.ecommerce.payments.domain.SaleId
+import com.ecommerce.payments.domain.*
 import com.fasterxml.jackson.annotation.JsonInclude
-import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
 fun Application.configureRouting(paymentService: PaymentService) {
     routing {
         post("/ecommerce/payment") {
             val requestBody = call.receive<PaymentRequestDTO>()
-            val response = paymentService.makeA(Payment(
-                saleId = SaleId(requestBody.saleId), amount = Amount.from(requestBody.amount,requestBody.currency)
-            ))
+            val payment: Deferred<PaymentResult> = async(Dispatchers.Default){
+                paymentService.makeA(Payment(
+                    saleId = SaleId(requestBody.saleId), amount = Amount.from(requestBody.amount,requestBody.currency)
+                ))
+            }
+            val response = payment.await()
             response.reference.fold(
-                { call.respond(status = OK, PaymentResponseDTO(reference = null, result = response.result, response.fraudScore)) },
-                { call.respond(status = OK, PaymentResponseDTO(reference = it.value, result = response.result, response.fraudScore)) }
+                { call.respond(status = HttpStatusCode.OK, PaymentResponseDTO(reference = null, result = response.result, response.fraudScore)) },
+                { call.respond(status = HttpStatusCode.OK, PaymentResponseDTO(reference = it.value, result = response.result, response.fraudScore)) }
             )
         }
     }
